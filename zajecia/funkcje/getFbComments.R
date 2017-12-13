41d92c9436f152badbba0c38becd2ac69e8864ac
Marcin Wilkowski <m@wilkowski.org>
2017-12-13 19:06:27 +0100

    Update getFbComments.R

--- zajecia/funkcje/getFbComments.R
+++ zajecia/funkcje/getFbComments.R
@@ -4,11 +4,9 @@ getFbComments <- function(id,z){
   # id - id postów
   # z - ile komentarzy ma zwrócić funkcja
   p <- as.data.frame(NULL)
-  a <- 1
   for(i in id){
     u <- getPost(i,token,n=z)
     c <- rbind(u$comments,p)
-    a <- a + 1
   }
   return(c)  
 }
