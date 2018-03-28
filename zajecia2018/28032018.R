b3e1d760d0785cc4873f8c13ab23b90c042c07cd
Marcin Wilkowski <m@wilkowski.org>
2018-03-28 17:51:43 +0200

    Update 28032018.R

--- zajecia2018/28032018.R
+++ zajecia2018/28032018.R
@@ -1,11 +1,20 @@
 
+
+sc <- read.csv('scihub/poland_scihub.csv')
 library(dplyr)
+
+#V2, V6, V7, V8
+
+sc <- sc[,-1]
+#sc <- sc[,c(-1,-3)]
 
 sc <- sc %>% select(V2,V6,V7,V8)
 
+names(sc)
+
+save(sc, file="scihub/sc.RData")
 
 names(sc) <- c("artykul","miejscowosc","lat","lon")
-save(sc, file="scihub/sc.RData")
 
 
 View(as.data.frame(table(sc$miejscowosc)))
@@ -21,6 +30,21 @@ str(unique(sc$miejscowosc))
 
 sc3 <- sc2 %>% group_by(miejscowosc) %>% summarise(pobrania = sum(liczba))
 
+
 #### usuwanie
 
 sc4 <- sc3 %>% filter(miejscowosc != "N/A")
+
+# dane gograficzne
+
+namiary <- sc2 %>%  group_by(miejscowosc) %>% summarise(lat1 = lat[1], lon1 = lon[1])
+
+namiary <- namiary %>% filter(miejscowosc != "N/A")
+
+
+#s <- 100:1000
+#s[1]
+
+dane <- inner_join(sc4,namiary)
+
+
