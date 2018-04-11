d87d5e7580d3b5ce151f69d4800ac8fa2b835042
Marcin Wilkowski <m@wilkowski.org>
2018-04-11 17:22:25 +0200

    Update r11042018.R

--- zajecia2018/r11042018.R
+++ zajecia2018/r11042018.R
@@ -12,11 +12,13 @@ temp <- adresy[1]
 library(xml2)
 t <- read_html(temp, encoding = "UTF-8")
 t <- xml_find_all(t, "//div[@class='td-post-content']")
-t <- gsub("<.*?>","", t)
+t <- gsub("<.*?>"," ", t)
 t <- gsub("\n"," ",t,fixed = TRUE)
 t <- gsub("\r"," ",t,fixed = TRUE)
 t <- gsub("\t"," ",t,fixed = TRUE)
-t <- gsub("„","",t, fixed = TRUE)
-t <- gsub("”","",t, fixed = TRUE)
+t <- gsub("„"," ",t, fixed = TRUE)
+t <- gsub("”"," ",t, fixed = TRUE)
 Encoding(t) <- 'UTF-8'
 write(t, file = "listy/test.txt")
+
+
