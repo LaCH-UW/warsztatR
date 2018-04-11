b430c90ccb5b500cc9a4a03e67ae7b525c59f462
Marcin Wilkowski <m@wilkowski.org>
2018-04-11 18:18:24 +0200

    Update r11042018.R

--- zajecia2018/r11042018.R
+++ zajecia2018/r11042018.R
@@ -66,4 +66,15 @@ korpus <- tm_map(korpus,removeNumbers)
 korpus <- tm_map(korpus,removePunctuation)
 korpus <- tm_map(korpus,content_transformer(gsub),pattern = "–", replacement = " ", fixed = TRUE)
 
+# prosta lematyzacja w R
+# https://wilkowski.org/notka/1587
 
+getLemma <- function(t,u) {
+  library(httr)
+  library(xml2)
+  p <- list(lpmn="any2txt|wcrft2",text=t,user=u)
+  s <- POST("http://ws.clarin-pl.eu/nlprest2/base/process", body = p, encode = "json", verbose())
+  r <- content(s, "text")
+  r <- gsub('[[:punct:] ]+','',unlist(as_list(xml_find_all(read_xml(r),"//base"))))
+  return(r[r != ""])
+}
