f59d9d361214b1d8d38643e04f4b77dc642ad850
Marcin Wilkowski <m@wilkowski.org>
2018-04-25 16:25:55 +0200

    Update 25042018.R

--- zajecia2018/25042018.R
+++ zajecia2018/25042018.R
@@ -1,3 +1,8 @@
 
 #warto przetestowac
 # https://cran.r-project.org/web/packages/SocialMediaLab/SocialMediaLab.pdf
+
+
+########################## twitteR
+# zakładamy aplikacje
+#https://apps.twitter.com/
